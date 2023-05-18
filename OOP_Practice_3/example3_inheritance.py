be7fb8001530194041bf1f192597a3ec1910b71a
Gemmus <decadentcrown@gmail.com>
2023-05-18 09:33:50 +0300

    Initial commit.

---
+++ OOP_Practice_3/example3_inheritance.py
@@ -0,0 +1,41 @@
+class Employee:
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+    def work(self):
+        print(f"{self.name} is working...")
+
+
+class SoftwareEngineer(Employee):
+    def __init__(self, name, age, salary, level):
+        super().__init__(name, age, salary)
+        self.level = level
+
+    def work(self):
+        print(f"{self.name} is coding...")
+
+    def debug(self):
+        print(f"{self.name} is debugging...")
+
+
+class Designer(Employee):
+    def __init__(self, name, age, salary):
+        super().__init__(name, age, salary)
+
+    def work(self):
+        print(f"{self.name} is designing...")
+
+    def draw(self):
+        print(f"{self.name} is drawing...")
+
+
+se = SoftwareEngineer("Paul", 35, "senior", 9000)
+se.work()
+se.debug()
+d = Designer("Philip", 25, 5000)
+d.work()
+d.draw()
+a = Employee("Daniel", 30, 4000)
+a.work()
